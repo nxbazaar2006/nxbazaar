f306fbd2df30dfd06bed3b2190c77f5d9c64128e
nxbazaar.v <nxbazaar.v@gmail.com>
2026-04-24 14:53:45 +0530

    update

--- app/(back-office)/dashboard/(catalogue)/subcategories/page.tsx
+++ app/(back-office)/dashboard/(catalogue)/subcategories/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import PageHeader from "@/components/backoffice/PageHeader";
 import SubCategoriesClient from "./SubCategoriesClient";
 import { getSubCategories } from "@/actions/subcategory";
@@ -15,21 +14,6 @@ export default async function SubCategoryPage() {
       />
 
       <SubCategoriesClient initialData={subCategories} />
-=======
-import { db } from "@/lib/db";
-import { columns } from "./columns";
-import DataTable from "@/components/DataTableComponents/DataTable";
-
-export default async function Page() {
-  const subCategories = await db.subCategory.findMany({
-    include: { translations: true },
-    orderBy: { createdAt: "desc" },
-  });
-
-  return (
-    <div className="p-4">
-      <DataTable columns={columns} data={subCategories} />
->>>>>>> cfe7124 (update)
     </div>
   );
 }
